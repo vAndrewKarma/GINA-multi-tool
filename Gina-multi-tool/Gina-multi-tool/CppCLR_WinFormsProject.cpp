658a6f377a1af950576d75738c267bf190762645
Your Name <you@example.com>
2024-11-09 17:32:08 +0200

    test

---
+++ Gina-multi-tool/Gina-multi-tool/CppCLR_WinFormsProject.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+
+using namespace System;
+
+// int main(array<System::String ^> ^args)
+// {
+//    return 0;
+// }
+
+#include "Form1.h"
+
+using namespace System::Windows::Forms;
+
+int main()
+{
+  Application::EnableVisualStyles();
+  Application::SetCompatibleTextRenderingDefault(false);
+  Application::Run(gcnew CppCLRWinFormsProject::Form1());
+  return 0;
+}
